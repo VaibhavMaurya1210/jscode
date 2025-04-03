82ef36d32f7a640ab2802bb668cb638ac658193f
vaibhavmaurya <mauryavaibhav780@gmail.com>
2025-04-03 18:10:20 +0000

    variables

---
+++ basicJs/variable.js
@@ -0,0 +1,19 @@
+const accountId = 12344
+let emailId = "Vaibhavmaurya344@gmail.com"
+var password = "12345"
+
+
+
+// console.log(accountId);
+// console.log(emailId);
+// console.log(password);
+
+
+/*
+    perefer not use to var 
+    because of issue block scope and functional scope
+*/
+
+
+console.table([accountId, emailId, password]);
+
